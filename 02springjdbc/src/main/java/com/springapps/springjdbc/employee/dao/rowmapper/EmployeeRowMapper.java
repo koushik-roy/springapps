97ef6db90cb417332a4d5419dbd8967bc76aa47e
Radhakrishnan Meiappan <rmeiappan@rocketsoftware.com>
2022-05-04 12:01:11 +0530

    changes

--- 02springjdbc/src/main/java/com/springapps/springjdbc/employee/dao/rowmapper/EmployeeRowMapper.java
+++ 02springjdbc/src/main/java/com/springapps/springjdbc/employee/dao/rowmapper/EmployeeRowMapper.java
@@ -7,7 +7,7 @@ import org.springframework.jdbc.core.RowMapper;
 
 import com.springapps.springjdbc.employee.dto.Employee;
 
-public class EmployeeRowMapper implements RowMapper<Employee>{
+public class EmployeeRowMapper implements RowMapper<Employee> {
 
 	@Override
 	public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
